7d1add5ed671cd8a9975309b190298bda49840ed
mpavlovic <mario@undabot.com>
2016-05-17 11:54:57 +0200

    Search algorithm
    Implemented recursive search algorithm for a circuit. We run down the (input) circuit and simply look if the output wire has inputs as values or other wires. If we find a value than we apply the (bitwise) operation and return the result. If we find a wire then we try (by the same algorithm) to find a value for that output wire. Basically, we just spin in circles trying to get values as inputs.

--- 2015/advent-of-code/day7.swift
+++ 2015/advent-of-code/day7.swift
@@ -22,6 +22,66 @@ class Day7 {
             outputWire = ""
         }
     }
+    
+    var outputs: [String : UInt16] = [:]
+    
+    func runCircut(resultOnWire resultOnWire: String, filePath: String) -> UInt16 {
+        let fileInput = readInput(filePath)
+        return outputSignal(onWire: resultOnWire, input: fileInput)
+    }
+}
+
+// MARK: - Finding value in a circuit
+extension Day7 {
+    func outputSignal(onWire onWire: String, input: [String]) -> UInt16 {
+        let model = parseInstructions(input: input)
+        return outputValue(onWire: onWire, model: model)
+    }
+    
+    func outputValue(onWire onWire: String, model: [Instruction]) -> UInt16 {
+        //check if it has it first
+        if let output = outputs[onWire] {
+            return output
+        }
+        
+        //find desired wire
+        guard let resultWireInstruction = (model.filter { instruction in
+            return instruction.outputWire == onWire
+            }.first) else { return 0 }
+        
+        //check what kind of inputs we have (values or wires)
+        var leftInput: UInt16
+        if UInt16(resultWireInstruction.leftWire) == nil {
+            leftInput = outputValue(onWire: resultWireInstruction.leftWire, model: model)
+        } else {
+            leftInput = UInt16(resultWireInstruction.leftWire)!
+        }
+        
+        var rightInput: UInt16
+        if UInt16(resultWireInstruction.rightWire) == nil {
+            rightInput = outputValue(onWire: resultWireInstruction.rightWire, model: model)
+        } else {
+            rightInput = UInt16(resultWireInstruction.rightWire)!
+        }
+        
+        var result: UInt16
+        switch resultWireInstruction.operation {
+        case .AND:
+            result = leftInput & rightInput
+        case .OR:
+            result = leftInput | rightInput
+        case .NOT:
+            result = ~rightInput
+        case .LSHIFT:
+            result = leftInput << rightInput
+        case .RSHIFT:
+            result = leftInput >> rightInput
+        }
+        
+        //store and return
+        outputs[onWire] = result
+        return result
+    }
 }
 
 // MARK: - File input
--- 2015/advent-of-codeTests/Day7Specs.swift
+++ 2015/advent-of-codeTests/Day7Specs.swift
@@ -1,5 +1,7 @@
 import Quick
 import Nimble
+@testable import advent_of_code
+
 
 class Day7Specs: QuickSpec {
     override func spec() {
@@ -25,8 +27,110 @@ class Day7Specs: QuickSpec {
                 it("should map input to instructions", closure: { 
                     let input = ["123 -> x", "x AND y -> d", "x OR y -> e", "x LSHIFT 2 -> f", "y RSHIFT 2 -> g", "NOT x -> h"]
                     let result = sut.parseInstructions(input: input)
-                    
                     expect(result.count).to(equal(6))
+                    
+                    let first = result.first!
+                    expect(first.leftWire).to(equal("123"))
+                    expect(first.outputWire).to(equal("x"))
+                    
+                    let last = result.last!
+                    expect(last.rightWire).to(equal("x"))
+                    expect(last.outputWire).to(equal("h"))
+                })
+            })
+            
+            describe("when running single operation systems", { 
+                it("should work correctly on direct value", closure: {
+                    var instruction = Day7.Instruction()
+                    instruction.leftWire = "3"
+                    instruction.operation = .AND
+                    instruction.rightWire = "3"
+                    instruction.outputWire = "x"
+                    
+                    let result = sut.outputValue(onWire: "x", model: [instruction])
+                    expect(result).to(equal(3))
+                })
+                
+                it("should work correctly on AND operation", closure: {
+                    var instruction = Day7.Instruction()
+                    instruction.leftWire = "123"
+                    instruction.operation = .AND
+                    instruction.rightWire = "456"
+                    instruction.outputWire = "x"
+                    
+                    let result = sut.outputValue(onWire: "x", model: [instruction])
+                    expect(result).to(equal(72))
+                })
+
+                it("should work correctly on OR operation", closure: {
+                    var instruction = Day7.Instruction()
+                    instruction.leftWire = "123"
+                    instruction.operation = .OR
+                    instruction.rightWire = "456"
+                    instruction.outputWire = "x"
+                    
+                    let result = sut.outputValue(onWire: "x", model: [instruction])
+                    expect(result).to(equal(507))
+                })
+
+                it("should work correctly on LSHIFT operation", closure: {
+                    var instruction = Day7.Instruction()
+                    instruction.leftWire = "123"
+                    instruction.operation = .LSHIFT
+                    instruction.rightWire = "2"
+                    instruction.outputWire = "x"
+                    
+                    let result = sut.outputValue(onWire: "x", model: [instruction])
+                    expect(result).to(equal(492))
+                })
+
+                it("should work correctly on RSHIFT operation", closure: {
+                    var instruction = Day7.Instruction()
+                    instruction.leftWire = "456"
+                    instruction.operation = .RSHIFT
+                    instruction.rightWire = "2"
+                    instruction.outputWire = "x"
+                    
+                    let result = sut.outputValue(onWire: "x", model: [instruction])
+                    expect(result).to(equal(114))
+                })
+
+                it("should work correctly on NOT operation", closure: {
+                    var instruction = Day7.Instruction()
+                    instruction.operation = .NOT
+                    instruction.rightWire = "123"
+                    instruction.outputWire = "x"
+                    
+                    let result = sut.outputValue(onWire: "x", model: [instruction])
+                    expect(result).to(equal(65412))
+                })
+            })
+            
+            describe("when running the circuit", { 
+                it("should give wire output value for desired wire", closure: { 
+                    let input = ["123 -> x", "456 -> y", "x AND y -> d", "x OR y -> e", "x LSHIFT 2 -> f", "y RSHIFT 2 -> g", "NOT x -> h", "NOT y -> i"]
+                    let d = sut.outputSignal(onWire: "d", input: input)
+                    let e = sut.outputSignal(onWire: "e", input: input)
+                    let f = sut.outputSignal(onWire: "f", input: input)
+                    let g = sut.outputSignal(onWire: "g", input: input)
+                    let h = sut.outputSignal(onWire: "h", input: input)
+                    let i = sut.outputSignal(onWire: "i", input: input)
+                    let x = sut.outputSignal(onWire: "x", input: input)
+                    let y = sut.outputSignal(onWire: "y", input: input)
+                    
+                    expect(d).to(equal(72))
+                    expect(e).to(equal(507))
+                    expect(f).to(equal(492))
+                    expect(g).to(equal(114))
+                    expect(h).to(equal(65412))
+                    expect(i).to(equal(65079))
+                    expect(x).to(equal(123))
+                    expect(y).to(equal(456))
+                })
+                
+                it("should find a solution for day 7 tasks", closure: { 
+                    let result = sut.runCircut(resultOnWire: "a", filePath: "day7")
+                    expect(result).to(equal(16076))
                 })
             })
         }
